d852d43570b476315c3d4c4bbdc35d10c448b678
Juan Manuel Barrios <jmbarrios.ar@outlook.com>
2024-04-24 11:45:06 -0300

    led blink updated

--- Test1/src/main.cpp
+++ Test1/src/main.cpp
@@ -4,15 +4,22 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(D6, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(D6, HIGH);
   delay(1000);
-  Serial.println("ENCENDIDO");
+  Serial.println("ENCENDIDO LED AMARILLO");
   digitalWrite(D6, LOW);
   delay(1000);
-  Serial.println("APAGADO");
+  Serial.println("APAGADO LED AMARILLO");
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  Serial.println("ENCENDIDO EL BUILTIN");
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+  Serial.println("APAGADO EL BUILTIN");
 }
 

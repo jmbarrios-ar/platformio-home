da137f68c987eac5df1f6f82a596e3ce479abafd
jmbarrios-ar <jmbarrios.ar@outlook.com>
2024-04-25 23:02:12 -0300

    actualizar

--- ESP8266_MQTT_Alarm_PIR/src/main.cpp
+++ ESP8266_MQTT_Alarm_PIR/src/main.cpp
@@ -1,13 +1,13 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 // Definir los parámetros de la red WiFi
-const char* ssid = "datacenter";
-const char* password = "NOv22$1nicI0";
+const char* ssid = "247IASbros2.4";
+const char* password = "T3reKByo2023$";
 
 // Definir los parámetros del broker MQTT
-const char* mqtt_server = "170.10.10.68";
+const char* mqtt_server = "192.168.24.150";
 const int mqtt_port = 1883; // Puerto predeterminado para MQTT
-const char* mqtt_user = "adminmqtt"; 
+const char* mqtt_user = "usermqtt";    // en Datacenter mqtt_user = "adminmqtt"
 const char* mqtt_password = "Ia$247";
 
 WiFiClient espClient;
@@ -17,7 +17,10 @@ PubSubClient client(espClient);
 const int pirPin = D7; // Por ejemplo, utilizar el pin GPIO 13
 
 // Definir el tópico MQTT para publicar el estado de la alarma
-const char* alarm_topic = "Datacenter/Alarm_01";
+const char* alarm_topic = "casa/alarm_01";
+
+int pirState = LOW;           // de inicio no hay movimiento
+int pirValue = 0;                  // estado del pin
 
 void setup_wifi() {
   delay(10);
@@ -68,7 +71,8 @@ void setup() {
   client.setCallback(callback);
 }
 
-void loop() {
+void loop() 
+{
   if (!client.connected()) {
     reconnect();
   }
@@ -79,10 +83,26 @@ void loop() {
 
   if (pirValue == HIGH) {
     // Publicar el estado de disparo de la alarma cuando se detecta movimiento
-    client.publish(alarm_topic, "Alarma activada - Movimiento detectado");
+    
     digitalWrite(D6, HIGH);
+    if (pirState == LOW)  //si previamente estaba apagado
+    {
+      client.publish(alarm_topic, "Alarma activada - Movimiento detectado");
+      Serial.println("Sensor activado");
+      pirState = HIGH;
+    }
     delay(1000); // Esperar un segundo antes de volver a publicar para evitar múltiples publicaciones consecutivas
+  
+  else   //si esta desactivado
+  {
+    digitalWrite(D6, LOW); // LED OFF
+    if (pirState == HIGH)  //si previamente estaba encendido
+    {
+      client.publish(alarm_topic, "Alarma desactivada - Sin Movimiento alguno");
+      Serial.println("Sensor parado");
+      pirState = LOW;
+    }
   }
-
   // Puedes agregar más lógica aquí según tus necesidades, como apagar la alarma después de cierto tiempo, etc.
+  }
 }
